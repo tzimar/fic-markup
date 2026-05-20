981c355430f01879bd225d677eb387228906e852
tzimar <275237023+tzimar@users.noreply.github.com>
2026-05-20 22:05:44 +1000

    Add test script and minimal script

--- render.py
+++ render.py
@@ -1,1 +1,20 @@
-# TODO
+import os
+import sys
+
+file_in = sys.argv[1]
+
+with open(file_in, 'r') as f:
+  input = f.read()
+
+file_out: str
+if len(sys.argv) > 2:
+  file_out = sys.argv[2] 
+else:
+  file_out = os.path.splitext(file_in)[0] + '.html'
+
+output: str = ''
+
+# TODO: Parse txt into HTML
+
+with open(file_out, 'w') as f:
+  f.write(output)
